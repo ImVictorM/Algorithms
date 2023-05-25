6a995ca73d2c8188baf81414b6203c48ea927c18
Victor Mendes <victorutdsim7@gmail.com>
2023-05-25 19:24:14 -0300

    [req-6]-feat: função is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    left, right = 0, len(word) - 1
+    for _ in word:
+        if word[right] != word[left]:
+            return False
+        else:
+            right -= 1
+            left += 1
+
+    return True
