64b0126cbd76ae129966f4b2437d6ed274b40761
Victor Mendes <victorutdsim7@gmail.com>
2023-05-25 14:50:36 -0300

    [req-1]-feat: função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    people_present_in_target_time = 0
+
+    for entry_time, exit_time in permanence_period:
+        if isinstance(entry_time, int) and isinstance(exit_time, int):
+            if entry_time <= target_time <= exit_time:
+                people_present_in_target_time += 1
+        else:
+            return None
+
+    return people_present_in_target_time
