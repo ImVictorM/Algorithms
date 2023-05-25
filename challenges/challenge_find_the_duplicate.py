270044a855fc03b258b62c9c77afcacd5f940b66
Victor Mendes <victorutdsim7@gmail.com>
2023-05-25 18:42:17 -0300

    [req-5]-feat: função find_duplicated e busca binária

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,34 @@
+def binary_search(lst, target, start=0):
+    end = len(lst) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if lst[mid] == target:
+            return (lst[mid], mid)
+
+        if target < lst[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return None
+
+
 def find_duplicate(nums):
-    """Faça o código aqui.."""
-    raise NotImplementedError
+    if not isinstance(nums, list):
+        return False
+
+    nums.sort
+
+    for index, num in enumerate(nums):
+        if not (isinstance(num, int) and num >= 0):
+            return False
+
+        next = index + 1
+        exists = binary_search(nums, num, next)
+
+        if exists:
+            return exists[0]
+
+    return False
